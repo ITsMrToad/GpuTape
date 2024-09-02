8ec8abb1bddd3885dc8923cfd2ba7f5f9eaffe83
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-09-02 23:10:26 +0500

    Remove toadlib

--- src/main/java/com/mr_toad/gpu_tape/client/error/CleanException.java
+++ src/main/java/com/mr_toad/gpu_tape/client/error/CleanException.java
@@ -1,11 +1,7 @@
 package com.mr_toad.gpu_tape.client.error;
 
-import com.mr_toad.lib.mtjava.strings.func.StringSupplier;
-
 public class CleanException extends IllegalStateException {
-
-    public CleanException(StringSupplier msg, Throwable cause) {
-        super(msg.getAsString(), cause);
+    public CleanException(String msg, Throwable cause) {
+        super(msg, cause);
     }
-
 }
