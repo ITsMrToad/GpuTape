41ee8c10dce6bc10f050c6e22c0c703295ca4eb8
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-12-29 22:38:59 +0500

    Move CleanException to util | 1.0.5

--- src/main/java/com/mr_toad/gpu_tape/client/error/CleanException.java
+++
@@ -1,7 +0,0 @@
-package com.mr_toad.gpu_tape.client.error;
-
-public class CleanException extends IllegalStateException {
-    public CleanException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-}
