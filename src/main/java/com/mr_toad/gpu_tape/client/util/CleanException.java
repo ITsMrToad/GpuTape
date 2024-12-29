074b9765271cbd07e967c5047169e4328d3d4951
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-12-29 22:39:39 +0500

    Move CleanException & VulkanCompat

---
+++ src/main/java/com/mr_toad/gpu_tape/client/util/CleanException.java
@@ -0,0 +1,10 @@
+package com.mr_toad.gpu_tape.client.util;
+
+
+public class CleanException extends IllegalStateException {
+
+    public CleanException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
