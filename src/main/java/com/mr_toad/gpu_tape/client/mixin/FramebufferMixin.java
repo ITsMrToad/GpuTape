1dfef099b42263339f49f81f26c71ca2c0a0a39b
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-12-29 22:43:35 +0500

    Vulkan compat | 1.0.5

--- src/main/java/com/mr_toad/gpu_tape/client/mixin/FramebufferMixin.java
+++ src/main/java/com/mr_toad/gpu_tape/client/mixin/FramebufferMixin.java
@@ -1,7 +1,7 @@
 package com.mr_toad.gpu_tape.client.mixin;
 
 import com.mr_toad.gpu_tape.client.GpuTape;
-import com.mr_toad.gpu_tape.client.error.CleanException;
+import com.mr_toad.gpu_tape.client.util.CleanException;
 import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
@@ -11,22 +11,42 @@ import org.spongepowered.asm.mixin.Shadow;
 import java.lang.ref.Cleaner;
 
 @Mixin(Framebuffer.class)
-public abstract class FramebufferMixin implements Cleaner.Cleanable {
+public abstract class FramebufferMixin implements FramebufferFixer, Cleaner.Cleanable {
 
     @Shadow protected int colorAttachment;
     @Shadow protected int depthAttachment;
     @Shadow public int fbo;
 
-    @Shadow @Final public boolean useDepthAttachment;
-
     @Override
     public void clean() {
         try {
-            if (this.colorAttachment > -1 || this.depthAttachment > -1 || this.fbo > -1) {
-                GpuTape.FRAMEBUFFERS.add(new Framebuffer(this.useDepthAttachment) {});
+            if (!GpuTape.isVulkan() && (this.colorAttachment > -1 || this.depthAttachment > -1 || this.fbo > -1)) {
+                GpuTape.FIXERS.add(this);
             }
         } catch (Exception e) {
             throw new CleanException("Failed to finalize/clean '" + this + "'", e);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        GlStateManager._bindTexture(0);
+        GlStateManager._glBindFramebuffer(36160, 0);
+    }
+
+    @Override
+    public void release() {
+        if (this.colorAttachment > -1) {
+            TextureUtil.releaseTextureId(this.colorAttachment);
+        }
+
+        if (this.depthAttachment > -1) {
+            TextureUtil.releaseTextureId(this.depthAttachment);
+        }
+
+        if (this.fbo > -1) {
+            GlStateManager._glBindFramebuffer(GlConst.GL_FRAMEBUFFER, 0);
+            GlStateManager._glDeleteFramebuffers(this.fbo);
         }
     }
 
