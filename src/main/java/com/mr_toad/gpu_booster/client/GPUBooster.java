a9ea5af8c6152638905f59e24500e0847d05c97d
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2025-12-04 21:46:50 +0500

    Fix resource reload | 1.1.0-beta5

--- src/main/java/com/mr_toad/gpu_booster/client/GPUBooster.java
+++ src/main/java/com/mr_toad/gpu_booster/client/GPUBooster.java
@@ -1,18 +1,17 @@
 package com.mr_toad.gpu_booster.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mr_toad.gpu_booster.api.GBGL;
+import com.mr_toad.gpu_booster.api.VertexFormatCacheAPI;
 import com.mr_toad.gpu_booster.client.config.DSAMode;
 import com.mr_toad.gpu_booster.client.config.GBConfig;
-import com.mr_toad.gpu_booster.client.rendering.gl.GBGL;
-import com.mr_toad.gpu_booster.client.rendering.gl.VertexFormatCache;
+import com.mr_toad.gpu_booster.client.resource.UnregisteredIdsResourceSupplier;
 import com.mr_toad.lib.api.config.ToadConfigs;
+import com.mr_toad.lib.core.ToadLibClient;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.minecraft.resource.ResourceManager;
+import net.minecraft.client.render.VertexFormats;
 import net.minecraft.resource.ResourceType;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.profiler.Profiler;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GLCapabilities;
 import org.slf4j.Logger;
@@ -25,8 +24,9 @@ public class GPUBooster implements ClientModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("GPUBooster");
     public static final String MODID = "gpu_booster";
-    public static final Identifier RENDER_CYCLE = Identifier.of(MODID, "render_cycle");
+
     public static final GBConfig CONFIG = new GBConfig();
+    public static final UnregisteredIdsResourceSupplier UNREGISTERED_IDS = new UnregisteredIdsResourceSupplier();
 
     public static boolean GL45;
     public static boolean DSA;
@@ -42,21 +42,21 @@ public class GPUBooster implements ClientModInitializer {
             if (CONFIG.hasDSA(DSAMode.VBO)) {
                 GBGL.preGen();
             }
+            LOGGER.info("GPUBooster caps initialized.");
         });
         ToadConfigs.create(MODID, CONFIG);
         System.setProperty("joml.fastmath", CONFIG.fastMath.get().toString());
-        ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new IdentifiableResourceReloadListener() {
-            @Override
-            public Identifier getFabricId() {
-                return RENDER_CYCLE;
-            }
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(UNREGISTERED_IDS);
+        if (CONFIG.vertexFormatCache.get()) {
+            VertexFormatCacheAPI.addCacheableFormats(VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL, VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL);
+            debug("Cacheable vertex formats added.");
+        }
+    }
 
-            @Override
-            public CompletableFuture<Void> reload(Synchronizer synchronizer, ResourceManager manager, Profiler prepareProfiler, Profiler applyProfiler, Executor prepareExecutor, Executor applyExecutor) {
-                VertexFormatCache.deleteUnregistered();
-                GPUBooster.LOGGER.debug("Clear unregistered VBO/EBO IDs.");
-                return null;
-            }
-        });
+    public static void debug(String msg) {
+        if (ToadLibClient.printDebug()) {
+            LOGGER.debug(msg);
+        }
     }
 }
+
